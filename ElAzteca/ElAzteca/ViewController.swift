7d44a7993173a28555e9bb23267cb41892dec097
yarixo <126021222+yarixo@users.noreply.github.com>
2023-02-21 14:46:53 -0600

    Add files via upload

---
+++ ElAzteca/ElAzteca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElAzteca
+//
+//  Created by Laboratorio11 UNAM-Apple11 on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
